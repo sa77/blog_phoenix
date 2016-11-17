a8d857e7cb45a0758b3100bf4ed8528c189eb857
sandeep <sandeep@vayu.com.au>
2016-11-17 12:19:57 +0545

    posts model

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :blog_phoenix, BlogPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "devuser",
+  password: "devuser",
   database: "blog_phoenix_dev",
   hostname: "localhost",
   pool_size: 10
